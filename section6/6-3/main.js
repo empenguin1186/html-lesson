86db7420a33bc3cdb9af54b270e6e41b8cec8d84
empenguin1186 <emperorpenguint1186@gmail.com>
2020-06-20 00:42:30 +0900

    :+1: js 基礎完了

--- section6/6-3/main.js
+++ section6/6-3/main.js
@@ -28,19 +28,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // インスタンス生成
     const ta1 = new TextAnimation('.animate-title');
-    const ta2 = new TextAnimation('.animate-title2');
+    const ta2 = new TweenTextAnimation('.tween-animate-title');
+    const btn = document.querySelector('.btn');
 
     // DOM生成後にアニメーションが行われる
     ta1.animate();
     ta2.animate();
+
+    // コールバックを ta1.animate のみにすると、this の値が btn となるため、メソッドがうまく機能しない
+    // animate メソッドは this が TextAnimation インスタンスの場合に機能するので、bind(ta1)が必要
+    btn.addEventListener('click', ta1.animate.bind(ta1));
+    btn.addEventListener('click', ta1.animate.bind(ta2));
 });
 
 class TextAnimation {
     constructor(el) {
-        this.el = document.querySelector(el);
-        this.chars = this.el.innerHTML.trim().split('');
-        this.el.innerHTML = this._getText();
-        this._addClickEvent()
+        this.DOM = {};
+        this.DOM.el = document.querySelector(el);
+        this.chars = this.DOM.el.innerHTML.trim().split('');
+        this.DOM.el.innerHTML = this._getText();
+        // this._addClickEvent()
     }
     
     // 1文字に分割し、spanタグを生成する
@@ -56,12 +63,45 @@ class TextAnimation {
         window.addEventListener('load', () => {
             const btn = document.querySelector('.btn');
             btn.addEventListener('click', () => {
-                this.el.classList.toggle('inview');
+                this.DOM.el.classList.toggle('inview');
             });
         });
     }
 
     animate() {
-        this.el.classList.toggle('inview');
+        // this が何を指しているかはスコープを考えるとわかる
+        // console.log(this);
+
+        // window.setTimeout(function() {
+        //     // この場合は window のスコープになるので、this は window となる
+        //     console.log(this);
+        //     this.el.classList.toggle('inview');
+        // }.bind(this)); // スコープの異なりによって this が指しているものが異なるので、一致させるためには bind メソッドを使用する
+        
+        // この場合は animate() のスコープ、すなわち TextAnimation クラスのメソッドのスコープなので、this は TextAnimation インスタンスとなる
+        this.DOM.el.classList.toggle('inview');
+    }
+}
+
+class TweenTextAnimation extends TextAnimation {
+
+    constructor(el) {
+        super(el);
+        this.DOM.chars = this.DOM.el.querySelectorAll('.char');
+        console.log(this.DOM.chars)
+    }
+
+    // 文字数関係なく delay を動的に設定できる。
+    animate() {
+        this.DOM.el.classList.toggle('inview');
+        this.DOM.chars.forEach((c, i) => {
+            TweenMax.to(c, .6, {
+                ease: Back.easeOut,
+                delay: i * .05,
+                startAt: {y: '-50%', opacity: 0},
+                y: '0%',
+                opacity: 1
+            })
+        });
     }
 }
