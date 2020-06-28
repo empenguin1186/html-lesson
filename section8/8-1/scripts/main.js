be492d93904ead15a835c97cc321d113c908d501
empenguin1186 <emperorpenguint1186@gmail.com>
2020-06-28 23:16:13 +0900

    :+1: footer, メニュー画面作成

--- section8/8-1/scripts/main.js
+++ section8/8-1/scripts/main.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const hero = new HeroSlider('.swiper-container');
-    hero.start({delay: 2000});
+    hero.start({ delay: 2000 });
     setTimeout(() => {
         hero.stop();
     }, 5000);
 
-    const cb = function(el, inview) {
-        if(inview) {
+    const cb = function (el, inview) {
+        if (inview) {
             ta = new TweenTextAnimation(el);
             ta.animate();
         }
-    }    
+    }
 
-    const so = new ScrollObserver('.tween-animate-title', cb, {once: true});
+    const so = new ScrollObserver('.tween-animate-title', cb, { once: true });
 
-    const _inviewAnimation = function(el, inview) {
+    const _inviewAnimation = function (el, inview) {
         if (inview) {
             el.classList.add('inview');
         } else {
@@ -23,4 +23,19 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const coverSlideObserver = new ScrollObserver('.cover-slide', _inviewAnimation, {});
+
+    const header = document.querySelector('.header');
+    const _navAnimation = function (el, inview) {
+        if (inview) {
+            // el.classList.remove('triggered');
+            header.classList.remove('triggered');
+        } else {
+            // el.classList.add('triggered');
+            header.classList.add('triggered');
+        }
+    }
+
+    const navigationObserver = new ScrollObserver('.nav-trigger', _navAnimation, { once: false });
+
+    new MobileMenu();
 })
